459540bbc55bda42e9435ec9e008ca9128f12344
Ashwani Kumar Vishwakarma <ashwanikumar3811@gmail.com>
2024-01-12 00:17:17 +0530

    added edit channel

--- app/api/channels/[channelId]/route.ts
+++ app/api/channels/[channelId]/route.ts
@@ -6,6 +6,49 @@ import { getCurrentProfile } from "@/lib/actions";
 import db from "@/lib/db";
 import { MemberRole } from "@prisma/client";
 
+export async function PATCH(req: Request, { params }: ChannelIdParams) {
+  try {
+    const profile = await getCurrentProfile();
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const { name, type } = await req.json();
+
+    const { searchParams } = new URL(req.url);
+    const serverId = searchParams.get("serverId");
+
+    if (!serverId) {
+      return new NextResponse("Server ID Missing", { status: 404 });
+    }
+
+    if (!params.channelId) {
+      return new NextResponse("Channel ID Missing", { status: 404 });
+    }
+
+    if (name == "general") {
+      return new NextResponse("Channel Name cannot be 'general'", {
+        status: 403,
+      });
+    }
+
+    const channel = await db.channel.update({
+      where: {
+        id: params.channelId,
+      },
+      data: {
+        name,
+        type,
+      },
+    });
+
+    return NextResponse.json(channel);
+  } catch (err) {
+    console.log("CHANNEL ID PATCH", err);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
+
 export async function DELETE(req: Request, { params }: ChannelIdParams) {
   try {
     const profile = await getCurrentProfile();
--- components/shared/ChannelForm.tsx
+++ components/shared/ChannelForm.tsx
@@ -132,7 +132,7 @@ const ChannelForm = ({
       <DialogContent className={"bg-white text-black p-0 overflow-hidden"}>
         <DialogHeader className={"pt-8 px-6"}>
           <DialogTitle className={"text-2xl text-center font-bold"}>
-            Create Channel
+            {channel ? "Edit" : "Create"} Channel
           </DialogTitle>
         </DialogHeader>
         <Form {...form}>
